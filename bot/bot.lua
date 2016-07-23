c4baa2753db965330f5256576046056bc583a0a6
MR_saf <jeiboor@users.noreply.github.com>
2016-07-23 07:41:22 +0430

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -50,12 +50,12 @@ function on_binlog_replay_end()
   load_plugins()
 end
 
-function msg_valid(msg)
+--function msg_valid(msg)
   -- Don't process outgoing messages
-  if msg.out then
-    print('\27[36mNot valid: msg from us\27[39m')
-    return false
-  end
+  --if msg.out then
+    --print('\27[36mNot valid: msg from us\27[39m')
+    --return false
+  --end
 
   -- Before bot was started
   if msg.date < os.time() - 5 then
@@ -78,10 +78,10 @@ function msg_valid(msg)
     return false
   end
 
-  if msg.from.id == our_id then
-    print('\27[36mNot valid: Msg from our id\27[39m')
-    return false
-  end
+  --if msg.from.id == our_id then
+    --print('\27[36mNot valid: Msg from our id\27[39m')
+    --return false
+  --end
 
   if msg.to.type == 'encr_chat' then
     print('\27[36mNot valid: Encrypted chat\27[39m')
